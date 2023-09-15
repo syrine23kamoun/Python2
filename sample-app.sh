377efd48c422fcd4044508793fdcc9aa34286c7e
syrine23kamoun <114397822+syrine23kamoun@users.noreply.github.com>
2023-09-16 01:07:45 +0200

    Add files via upload

---
+++ sample-app.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+rm -rf tempdir
+mkdir tempdir
+rm -rf tempdir/templates
+mkdir tempdir/templates
+rm -rf tempdir/static
+mkdir tempdir/static
+
+cp sample_app.py tempdir/.
+cp -r templates/* tempdir/templates/.
+cp -r static/* tempdir/static/.
+
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "EXPOSE 5050" >> tempdir/Dockerfile
+echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
+
+cd tempdir
+docker build -t sampleapp .
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a 
